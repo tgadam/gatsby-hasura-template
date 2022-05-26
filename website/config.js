0991970863ff89774751ff1c4402c210889acb0b
Adam Thomas <adam@teselagen.com>
2022-05-26 10:21:22 -0400

    adjusting template

--- website/config.js
+++ website/config.js
@@ -7,7 +7,7 @@ const config = {
   },
   header: {
     logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
-    logoLink: 'https://hasura.io/learn/',
+    logoLink: '/',
     title:
       "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
     githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
---
+++ website/content/about.md
@@ -0,0 +1,9 @@
+---
+title: 'About'
+metaTitle: 'About is the meta title tag for this page'
+metaDescription: 'This is the meta description for this page'
+---
+
+# About
+
+We're going to talk about stuff here
